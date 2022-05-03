604ada5e2026fe31d5f33b0a929ff2a9e6afa8b7
Ананьев Сергей <9852407799@mail.ru>
2022-05-03 08:20:09 +0300

    реализация презентера

--- src/main.js
+++ src/main.js
@@ -1,29 +1,6 @@
-import NewTripEventView from './view/new-trip-event-view.js';
-import TripEventEditView from './view/trip-event-edit-view.js';
-import TripSortView from './view/trip-sort-view.js';
-import TripFiltersView from './view/trip-filter-view.js';
-import DestinationView from './view/destination-view.js';
-import {render} from './render.js';
+import TripPresenter from './presenter/trip-presenter.js';
 
-// ------------ Отрисовка фильтров --------------
-const siteHeaderElement = document.querySelector('.trip-main');
-const tripControls = siteHeaderElement.querySelector('.trip-main__trip-controls');
-render(new TripFiltersView(), tripControls);
+const presenter = new TripPresenter();
 
-// ------------ Отрисовка сортировки ----------
-const tripEvents = document.querySelector('.trip-events');
-render(new TripSortView(), tripEvents);
+presenter.init();
 
-// --------------Отрисовка формы редактироввания --------
-const pageBody = document.querySelector('.page-body__page-main');
-const pageBodyContainer = pageBody.querySelector('.page-body__container');
-const tripEventsList = document.createElement('ul');
-const eventList = pageBodyContainer.appendChild(tripEventsList);
-eventList.classList.add('trip-events__list');
-render(new TripEventEditView(), eventList);
-
-// ------------- Отрисовка формы создания ----------
-render(new NewTripEventView(), eventList);
-
-// -------------Отрисовка точки маршрута ---------------
-render(new DestinationView(), eventList);
--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,33 +1,38 @@
-import BoardView from '../view/board-view.js';
-import SortView from '../view/sort-view.js';
-import TaskListView from '../view/task-list-view.js';
-import TaskView from '../view/task-view.js';
-import TaskEditView from '../view/task-edit-view.js';
-import LoadMoreButtonView from '../view/load-more-button-view.js';
-
-/*import NewTripEventView from './view/new-trip-event-view.js';
-import TripEventEditView from './view/trip-event-edit-view.js';
-import TripSortView from './view/trip-sort-view.js';
-import TripFiltersView from './view/trip-filter-view.js';
-import DestinationView from './view/destination-view.js';*/
+import NewTripEventView from '../view/new-trip-event-view.js';
+import TripEventEditView from '../view/trip-event-edit-view.js';
+import TripSortView from '../view/trip-sort-view.js';
+import TripFiltersView from '../view/trip-filter-view.js';
+import DestinationView from '../view/destination-view.js';
+import TripEventsListView from '../view/trip-events-list-view.js';
 import {render} from '../render.js';
 
+const ITEMQUANTITY = 3;
+
 export default class TripPresenter {
-  boardComponent = new BoardView();
-  taskListComponent = new TaskListView();
 
-  init = (boardContainer) => {
-    this.boardContainer = boardContainer;
+  init = () => {
+  // ------------ Отрисовка фильтров --------------
+    const siteHeaderElement = document.querySelector('.trip-main');
+    const tripControls = siteHeaderElement.querySelector('.trip-main__trip-controls');
+    render(new TripFiltersView(), tripControls);
 
-    render(this.boardComponent, this.boardContainer);
-    render(new SortView(), this.boardComponent.getElement());
-    render(this.taskListComponent, this.boardComponent.getElement());
-    render(new TaskEditView(), this.taskListComponent.getElement());
+    // ------------ Отрисовка сортировки ----------
+    const tripEvents = document.querySelector('.trip-events');
+    render(new TripSortView(), tripEvents);
 
-    for (let i = 0; i < 3; i++) {
-      render(new TaskView(), this.taskListComponent.getElement());
+    // -------------Отрисовка контейнера форм создания и редактирования -------
+    render(new TripEventsListView(), tripEvents);
+
+    // --------------Отрисовка формы редактироввания --------
+    const listContainer = tripEvents.querySelector('.trip-events__list');
+    render(new TripEventEditView(), listContainer);
+
+    // ------------- Отрисовка формы создания ----------
+    render(new NewTripEventView(), listContainer);
+
+    // -------------Отрисовка точки маршрута ---------------
+    for (let i = 0; i < ITEMQUANTITY; i++) {
+      render(new DestinationView(), listContainer);
     }
-
-    render(new LoadMoreButtonView(), this.boardComponent.getElement());
   };
 }
